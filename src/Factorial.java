a2188b1d78bc418598b65dd4c3cadc1d0a25b115
AlbinoJ <jesusdavida.e2089@gmail.com>
2022-09-28 07:07:33 -0700

    Factorial Threads

--- src/Factorial.java
+++ src/Factorial.java
@@ -6,13 +6,17 @@ public class Factorial implements Runnable {
         this.list = a;
     }
     public void run(){
-        int resultado = 0;
+        int resultado = 1;
         for (int i = 0; i < list.length; i++){
-            if (list[i] != 0){
-                for (int j = 1; j < i; j++){
-                    resultado = resultado * i;
+            if (list[i] != 0 && list[i] > 0) {
+                for (int j = 1; j < list[i]; j++) {
+                    resultado = resultado * j;
                 }
                 System.out.println(Thread.currentThread().getName() + ". El resultado para este Thread con factor de " + i + " es: " + resultado);
+            } else if (list[i] == 0){
+                System.out.println(Thread.currentThread().getName() + ". El resultado para este Thread con factor de " + i + " es: " + 1);
+            } else {
+                System.out.println("Para el Thread: " + Thread.currentThread().getName() + " no hay factorial");
             }
         }
 
