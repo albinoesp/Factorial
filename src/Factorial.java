cbf7f884ed8fc240ad175f8e1913012395400ac6
AlbinoJ <jesusdavida.e2089@gmail.com>
2022-09-28 07:00:07 -0700

    Factorial Threads

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,5 +1,20 @@
 public class Factorial implements Runnable {
+
+    int[] list;
+
+    public Factorial(int[] a){
+        this.list = a;
+    }
     public void run(){
+        int resultado = 0;
+        for (int i = 0; i < list.length; i++){
+            if (list[i] != 0){
+                for (int j = 1; j < i; j++){
+                    resultado = resultado * i;
+                }
+                System.out.println(Thread.currentThread().getName() + ". El resultado para este Thread con factor de " + i + " es: " + resultado);
+            }
+        }
 
     }
 }
--- src/Factorialmain.java
+++ src/Factorialmain.java
@@ -1,27 +1,18 @@
-import java.util.ArrayList;
 
 public class Factorialmain {
-    public int[] list;
-    public Factorialmain(int[] list) {
-        this.list = list;
-    }
     public static void main(String[] args) {
         int[] a ;
         a = new int[args.length];
+
         for(int i = 0; i < args.length; i++){
             a[i] = Integer.parseInt(args[i]);
         }
-        Factorialmain array = new Factorialmain(a);
 
-        Factorial f = new Factorial();
-        Thread t1 = new Thread();
-        Thread t2 = new Thread(f);
+        Thread t1 = new Thread(new Factorial(a));
+        Thread t2 = new Thread(new Factorial(a));
         t1.setName("+Thread 1+");
         t2.setName("+Thread 2+");
         t1.start();
         t2.start();
     }
-
-
-
 }
